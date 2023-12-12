62e193d03c81bcbe46b62f904f22a6247ef8499e
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2023-12-12 23:17:54 +0800

    Add the code

---
+++ BinarySearch.java
@@ -0,0 +1,35 @@
+public class BinarySearch
+{
+    public static boolean binarySearch(int[] nums, int target)
+    {
+         int left = 0, right = nums.length - 1, mid;
+
+         while(left <= right)
+         {
+             mid = left + (right - left) / 2;
+
+             if(nums[mid] > target)
+             {
+                right = mid - 1;
+             }
+             else if(nums[mid] < target)
+             {
+                left = mid + 1;
+             }
+             else
+             {
+                return true;
+             }
+         }
+
+         return false;
+    }
+
+    
+    public static void main(String[] args) 
+    {
+         int[] nums = {0,2,3,4,33,47};
+         int target = 2;   
+         System.out.println(binarySearch(nums, target));
+    }
+}
